61cedab5f5a7f2a061b3f7a00ccfcf52de1db761
Khalil <khalilhenane@hotmail.com>
2024-08-14 00:41:31 +0200

    worked on the display but doesn't work yet

---
+++ button.cpp
@@ -0,0 +1,25 @@
+// #include <Arduino.h>
+// #include "button.h"
+
+
+// const int buttonPin = 23;
+// // bool buttonvalue;
+
+// // int oldButtonValue = LOW; 
+// // bool turnOffBeep = false;
+
+// void setupButton() {
+//     pinMode(buttonPin, INPUT_PULLUP);  // Set up the button pin
+// }
+
+
+// bool buttonPress() {
+
+// if (digitalRead(buttonPin)  == LOW) {
+// Serial.println("Button is pressed");
+//   return true;
+
+// }
+
+// return false;    
+// }
